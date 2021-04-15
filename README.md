9f7155abc07e11d90a2f1032a577806bd1770220
usigna <bukazaokna@gmail.com>
2021-04-15 22:49:23 +0200

    Add mobile nav

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # yoon-group
+
+Time:
+* base files (normalize, fonts, etc.): 15 min
+* skip links: 5 min
+* scrollbar: 5 min
+* header (and nav):
+* hero:
+* our values:
+* portfolio: 
--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 function changeTitle() {
-  const message = "Back to Yoon Group!";
+  const message = 'Back to Yoon Group!';
   let original;
 
-  window.addEventListener("focus", function () {
+  window.addEventListener('focus', function () {
     if (original) {
       document.title = original;
     }
   })
 
-  window.addEventListener("blur", function () {
+  window.addEventListener('blur', function () {
     const title = document.title;
     if (title != message) {
       original = title;
@@ -18,29 +18,51 @@ function changeTitle() {
 }
 
 function showHamburgerMenu() {
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".navigation");
-  const navigationLinks = document.querySelectorAll(".navigation__link");
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.navigation');
+  const navigationLinks = document.querySelectorAll('.navigation__link');
+  const logo = document.querySelector('.logo');
 
   const handleClick = function () {
-    hamburger.classList.toggle("hamburger--active");
-    hamburger.setAttribute("aria-expanded", hamburger.classList.contains("hamburger--active"));
-    nav.classList.toggle("navigation--active");
+    hamburger.classList.toggle('hamburger--active');
+    hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
+    nav.classList.toggle('navigation--active');
+    logo.classList.toggle('logo--nav');
   }
 
   for (let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener("click", function () {
-      hamburger.classList.remove("hamburger--active");
-      nav.classList.remove("navigation--active");
+    navigationLinks[i].addEventListener('click', function () {
+      hamburger.classList.remove('hamburger--active');
+      nav.classList.remove('navigation--active');
+      logo.classList.remove('logo--nav');
     })
   }
 
-  hamburger.addEventListener("click", handleClick);
+  hamburger.addEventListener('click', handleClick);
+}
+
+function showHeader() {
+  const header = document.querySelector('.header');
+  const logo = document.querySelector('.logo');
+  const hamburger = document.querySelector('.hamburger');
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+      logo.classList.add('scrolled');
+      hamburger.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+      logo.classList.remove('scrolled');
+      hamburger.classList.remove('scrolled');
+    }
+  })
 }
 
 const init = function () {
   changeTitle();
   showHamburgerMenu();
+  showHeader();
 };
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
